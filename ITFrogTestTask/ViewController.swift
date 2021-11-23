ad0daabd8f17d17c630dbd9788e54f0ce22159c3
Gol-D <danya_golovin@icloud.com>
2021-11-23 11:28:58 +0300

    Initial Commit

---
+++ ITFrogTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ITFrogTestTask
+//
+//  Created by Daniel Golovin on 23.11.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
